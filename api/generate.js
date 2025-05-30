73b91a2d4f26d51ef6e5460f7a82dbe8497b467a
BigLongs <ggholafort3@gmail.com>
2025-05-30 20:40:25 -0300

    mudanças4

--- api/generate.js
+++ api/generate.js
@@ -1,16 +1,22 @@
-import fs from "fs"
-import path from "path"
 import crypto from "crypto"
 
-export default function handler(req, res) {
-  const filePath = path.resolve("./keys.json")
-  const db = JSON.parse(fs.readFileSync(filePath, "utf8"))
-
+export default async function handler(req, res) {
   const key = crypto.randomBytes(6).toString("hex")
 
-  db[key] = { used: false, hwid: null }
-
-  fs.writeFileSync(filePath, JSON.stringify(db, null, 2))
+  await fetch("https://hmbkknxewroraztzzqhr.supabase.co/rest/v1/keys", {
+    method: "POST",
+    headers: {
+      apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtYmtrbnhld3JvcmF6dHp6cWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NDcxNTAsImV4cCI6MjA2NDIyMzE1MH0.T4y4NKHrpKrQOiaZZt7_N1jbii75NmuAELL6hJiBJtI",
+      Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtYmtrbnhld3JvcmF6dHp6cWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NDcxNTAsImV4cCI6MjA2NDIyMzE1MH0.T4y4NKHrpKrQOiaZZt7_N1jbii75NmuAELL6hJiBJtI",
+      "Content-Type": "application/json",
+      Prefer: "return=minimal"
+    },
+    body: JSON.stringify({
+      id: key,
+      used: false,
+      hwid: null
+    })
+  })
 
   res.send(key)
 }
--- api/validate.js
+++ api/validate.js
@@ -1,16 +1,30 @@
-import fs from "fs"
-import path from "path"
-
 export default async function handler(req, res) {
   const { key, hwid } = req.query
-  const filePath = path.resolve("./keys.json")
-  const db = JSON.parse(fs.readFileSync(filePath, "utf8"))
 
-  if (!db[key]) return res.send("invalid_key")
+  const headers = {
+    apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtYmtrbnhld3JvcmF6dHp6cWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NDcxNTAsImV4cCI6MjA2NDIyMzE1MH0.T4y4NKHrpKrQOiaZZt7_N1jbii75NmuAELL6hJiBJtI",
+    Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtYmtrbnhld3JvcmF6dHp6cWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg2NDcxNTAsImV4cCI6MjA2NDIyMzE1MH0.T4y4NKHrpKrQOiaZZt7_N1jbii75NmuAELL6hJiBJtI"
+  }
 
-  if (!db[key].used) {
-    db[key].used = true
-    db[key].hwid = hwid
+  const getKey = await fetch(`https://hmbkknxewroraztzzqhr.supabase.co/rest/v1/keys?id=eq.${key}`, {
+    method: "GET",
+    headers: headers
+  })
+
+  const data = await getKey.json()
+  const k = data[0]
+
+  if (!k) return res.send("invalid_key")
+
+  if (!k.used) {
+    await fetch(`https://hmbkknxewroraztzzqhr.supabase.co/rest/v1/keys?id=eq.${key}`, {
+      method: "PATCH",
+      headers: {
+        ...headers,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ used: true, hwid: hwid })
+    })
 
     await fetch("https://discord.com/api/webhooks/1378152906196521052/p-rxCOQ8NYiAPwwZZd8hKprhjF253Pkrvz6p2QDwf-sZB79Y2xfLd0IzOtV593BtLxms", {
       method: "POST",
@@ -20,12 +34,10 @@ export default async function handler(req, res) {
       })
     })
 
-    fs.writeFileSync(filePath, JSON.stringify(db, null, 2))
-
     return res.send("valid")
   }
 
-  if (db[key].used && db[key].hwid === hwid) return res.send("valid")
+  if (k.hwid === hwid) return res.send("valid")
 
   return res.send("hwid_mismatch")
 }
